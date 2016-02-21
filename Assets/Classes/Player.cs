e1de97f0bbd84268253a4412f3fcd033cd6de425
vicra <vicramirez_10@hotmail.com>
2016-02-21 00:24:12 -0600

    fixing posible conflicts

--- Assets/Classes/Player.cs
+++ Assets/Classes/Player.cs
@@ -1,40 +1,37 @@
-﻿    using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Assets.Classes
+public class Player
 {
-    public class Player
+    private int Location { get; set; }
+    private int Money { get; set; }
+    private bool Imprisoned { get; set; }
+
+
+    void MoveLocation(int spaces)
     {
-        private int Location { get; set; }
-        private int Money { get; set; }
-        private bool Imprisoned { get; set; }
+        this.Location += spaces;
+    }
 
+    void AddMoney(int amount)
+    {
+        this.Money += amount;
+    }
 
-        void MoveLocation(int spaces)
-        {
-            this.Location += spaces;
-        }
+    void SubMoney(int amount)
+    {
+        this.Money -= amount;
+    }
 
-        void AddMoney(int amount)
-        {
-            this.Money += amount;
-        }
+    void FreeFromPrison()
+    {
+        this.Imprisoned = false;
+    }
 
-        void SubMoney(int amount)
-        {
-            this.Money -= amount;
-        }
-
-        void FreeFromPrison()
-        {
-            this.Imprisoned = false;
-        }
-
-        void Imprison()
-        {
-            this.Imprisoned = true;
-        }
+    void Imprison()
+    {
+        this.Imprisoned = true;
     }
 }
