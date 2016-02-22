738fff6176f2b7e5cf10c19e0e29488dc1ff41e2
vicra <vicramirez_10@hotmail.com>
2016-02-22 11:18:43 -0600

    last commit

--- Assets/Classes/Player.cs
+++ Assets/Classes/Player.cs
@@ -24,7 +24,7 @@ namespace Assets.Classes
 
         void SubMoney(int amount)
         {
-            this.Money -= amount;
+            this.Money -= amount;   
         }
 
         void FreeFromPrison()
